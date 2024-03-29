9cf0c218871e6d900abc06598cda8734eea18bb7
OksanaLesjuk <126483028+OksanaLesjuk@users.noreply.github.com>
2024-03-29 12:21:26 +0200

    page marcup

--- src/pages/HomeWaterPage.jsx
+++ src/pages/HomeWaterPage.jsx
@@ -1,4 +1,20 @@
+import styled from 'styled-components';
+import TodayWaterList from '../components/HomePageComponents/TodayWaterList';
+import MonthStateTable from '../components/HomePageComponents/MonthStateTable';
+
 const HomeWaterPage = () => {
+  const HomePageContainer = styled.div`
+    display: flex;
+  `;
+  const LeftBoxWrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+
+    width: 592px;
+    height: 680px;
+    padding: 32px 24px;
+  `;
+
   const RightBoxWrapper = styled.div`
     display: flex;
     flex-direction: column;
@@ -9,13 +25,13 @@ const HomeWaterPage = () => {
   `;
 
   return (
-    <>
-      <div></div>
+    <HomePageContainer>
+      <LeftBoxWrapper></LeftBoxWrapper>
       <RightBoxWrapper>
         <TodayWaterList></TodayWaterList>
         <MonthStateTable></MonthStateTable>
       </RightBoxWrapper>
-    </>
+    </HomePageContainer>
   );
 };
 
