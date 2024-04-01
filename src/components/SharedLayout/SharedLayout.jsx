5527c41662478330b2821e4c86c43e6a22bfdf4b
GeorgiySergeev <126503204+GeorgiySergeev@users.noreply.github.com>
2024-04-01 21:48:49 +0300

    commit#

--- src/components/SharedLayout/SharedLayout.jsx
+++ src/components/SharedLayout/SharedLayout.jsx
@@ -2,7 +2,7 @@ import { Suspense } from 'react';
 import { Outlet } from 'react-router-dom';
 import { AppNav } from '../AppNav/AppNav';
 
-import Header from '../Header/Header';
+// import Header from '../Header/Header';
 
 import { Footer } from '../Footer/Footer';
 // 382ce7f6cc54f25cdce456d3ca7c10b1f211379d
@@ -10,7 +10,7 @@ import { Footer } from '../Footer/Footer';
 const SharedLayout = () => {
   return (
     <>
-      <Header />
+      {/* <Header /> */}
       <AppNav />
       <Suspense fallback={null}>
         <Outlet />
