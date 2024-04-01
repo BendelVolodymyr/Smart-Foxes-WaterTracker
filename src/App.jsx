daad5e86718a45d7d6d482317450166044564daf
NataFilina <filinanatash@gmail.com>
2024-04-01 23:20:51 +0300

    loader
    додала шрифт для лоадера в fonts.css
    в App.js Suspense і Loader

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import { Route, Routes } from 'react-router-dom';
-import { lazy, useState } from 'react';
+import { Suspense, lazy } from 'react';
 import { RestrictedRoute } from './RestrictedRoute';
 
 import SharedLayout from 'components/SharedLayout/SharedLayout';
 import HomeWaterPage from './pages/HomeWaterPage';
-import { Main } from './components/Main/Main';
-
+import { Loader } from './components/Loader/Loader.jsx';
+// const Loader = lazy(() => import('./components/Loader/Loader.jsx'));
+const Main = lazy(() => import('./pages/Main/Main.jsx'));
 const HomeNotAuthPage = lazy(() => import('./pages/HomeNotAuth'));
 const LoginPage = lazy(() => import('./pages/LoginPage/LoginPage.jsx'));
 const Registration = lazy(() => import('./pages/RegistrationPage.jsx'));
@@ -23,34 +24,39 @@ function App() {
   // isRefreshing ? 'spiner' : усе що знизу
 
   return (
-    <Routes>
-      <Route path="/" element={<SharedLayout />}>
-        <Route index element={<HomeNotAuthPage />} />
-        <Route path="/welcome" element={<Main />} />
-        <Route
-          path="/login"
-          element={
-            <RestrictedRoute
-              redirectTo="/Smart-Foxes-WaterTracker/home"
-              component={<LoginPage />}
-            />
-          }
-        />
-        <Route
-          path="/registration"
-          element={
-            <RestrictedRoute redirectTo="/home" component={<Registration />} />
-          }
-        />
-        <Route
-          path="/home"
-          element={
-            <RestrictedRoute redirectTo="/" component={<HomeWaterPage />} />
-          }
-        />
-      </Route>
-      <Route path="*" element={<NotFoundPage />} />
-    </Routes>
+    <Suspense fallback={<Loader />}>
+      <Routes>
+        <Route path="/" element={<SharedLayout />}>
+          <Route index element={<HomeNotAuthPage />} />
+          <Route path="/welcome" element={<Main />} />
+          <Route
+            path="/login"
+            element={
+              <RestrictedRoute
+                redirectTo="/Smart-Foxes-WaterTracker/home"
+                component={<LoginPage />}
+              />
+            }
+          />
+          <Route
+            path="/registration"
+            element={
+              <RestrictedRoute
+                redirectTo="/home"
+                component={<Registration />}
+              />
+            }
+          />
+          <Route
+            path="/home"
+            element={
+              <RestrictedRoute redirectTo="/" component={<HomeWaterPage />} />
+            }
+          />
+        </Route>
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </Suspense>
   );
 }
 export default App;
