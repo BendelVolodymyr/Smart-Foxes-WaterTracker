9e4a977680d920576d3f0cc33b627ec2f584d523
OksanaLesjuk <126483028+OksanaLesjuk@users.noreply.github.com>
2024-04-03 17:23:33 +0300

    handler addWater btn

--- src/components/HomeWaterPageComponents/TodayWaterList/TodayWaterList.jsx
+++ src/components/HomeWaterPageComponents/TodayWaterList/TodayWaterList.jsx
@@ -20,7 +20,7 @@ import { ModalContext } from '../../../context';
 
 const TodayWaterList = ({ handleAddWaterClick }) => {
   //заглушка. замінити на дані зі стейту
-  const waterPortions = [
+  const waterDayList = [
     { id: 1, portion: '250', date: '2024-03-30T20:00:00.000Z' },
     { id: 2, portion: '250', date: '2024-03-30T10:00:00.000Z' },
     { id: 3, portion: '250', date: '2024-03-30T15:00:00.000Z' },
@@ -30,7 +30,6 @@ const TodayWaterList = ({ handleAddWaterClick }) => {
   ];
 
   const { openModal } = useContext(ModalContext);
-
   const [selectedPortion, setSelectedPortion] = useState(null);
 
   //заглушки для хендлерів
@@ -56,9 +55,9 @@ const TodayWaterList = ({ handleAddWaterClick }) => {
   return (
     <TodayBoxWrapper>
       <h3>Today</h3>
-      {waterPortions && waterPortions.length > 0 && (
+      {waterDayList && waterDayList.length > 0 && (
         <PortionsList>
-          {waterPortions.map((portion) => (
+          {waterDayList.map((portion) => (
             <li key={portion.id}>
               <ListContext>
                 <GlassSvg />
