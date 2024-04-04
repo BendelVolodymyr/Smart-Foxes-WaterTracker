8a266ad1beafc73505ba2dc7fb6b8cf022d01704
Tania <113130993+Tania66@users.noreply.github.com>
2024-04-04 15:45:32 +0300

    some change style

--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -14,6 +14,7 @@ const {handleShowPassword, toggleIcon, type} = useVisiblePassword();
 const bottle = bottleImg;
 const dispatch = useDispatch();
 
+//commit
 
 const SigninSchema = Yup.object().shape({
   email: Yup.string().email('Invalid email').required('Email is required'),
