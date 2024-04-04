2e8bc6c94e557896d621d657a524b14d5ee4fa90
BendelVolodymyr <bendel.vova@gmail.com>
2024-04-04 11:27:52 +0200

    persist

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,4 +1,36 @@
 module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+    'plugin:react-hooks/recommended',
+    'prettier',
+    'plugin:prettier/recommended',
+  ],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: ['.eslintrc.{js,cjs}'],
+      parserOptions: {
+        sourceType: 'script',
+      },
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react'],
+  rules: {},
+};
+
+module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
