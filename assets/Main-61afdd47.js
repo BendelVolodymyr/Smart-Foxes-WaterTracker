b908302c882bc62d37a617bd31a11dc5ca03bfec
BendelVolodymyr <134164183+BendelVolodymyr@users.noreply.github.com>
2024-04-05 13:27:28 +0000

    Deploying to gh-pages from @ BendelVolodymyr/Smart-Foxes-WaterTracker@f6ddc8497725297675b312064b2c6a8f5c97f3ad 🚀

--- assets/Main-61afdd47.js
+++ assets/Main-61afdd47.js
@@ -1,4 +1,4 @@
-import{n as t,G as d,j as e,H as l,a as x,L as p,F as n}from"./index-08674648.js";const h=t.div`
+import{n as t,G as d,j as e,H as l,a as x,L as p,F as i}from"./index-b1d93bb4.js";const h=t.div`
   display: flex;
   flex-direction: column;
   width: 280px;
@@ -54,7 +54,7 @@ import{n as t,G as d,j as e,H as l,a as x,L as p,F as n}from"./index-08674648.js
   font-style: normal;
   font-weight: 500;
   line-height: 111.111%;
-`,k=t.ul`
+`,b=t.ul`
   display: flex;
   gap: 16px;
   flex-direction: column;
@@ -70,7 +70,7 @@ import{n as t,G as d,j as e,H as l,a as x,L as p,F as n}from"./index-08674648.js
     flex-direction: column;
     align-items: start;
   }
-`,r=t.li`
+`,o=t.li`
   width: 216px;
   display: flex;
   gap: 8px;
@@ -83,7 +83,7 @@ import{n as t,G as d,j as e,H as l,a as x,L as p,F as n}from"./index-08674648.js
   @media screen and (min-width: 1440px) {
     width: 248px;
   }
-`,o=t.div`
+`,r=t.div`
   width: 32px;
   height: 32px;
   @media only screen and (min-width: 768px) {
@@ -97,7 +97,7 @@ import{n as t,G as d,j as e,H as l,a as x,L as p,F as n}from"./index-08674648.js
   font-style: normal;
   font-weight: 400;
   line-height: 125%;
-`,b=t.button`
+`,k=t.button`
   width: 280px;
   padding: 8px 30px;
   border-radius: 10px;
@@ -122,7 +122,7 @@ import{n as t,G as d,j as e,H as l,a as x,L as p,F as n}from"./index-08674648.js
   @media screen and (min-width: 1440px) {
     width: 384px;
   }
-`;function y(c){return d({tag:"svg",attr:{viewBox:"0 0 512 512"},child:[{tag:"rect",attr:{width:"416",height:"384",x:"48",y:"80",fill:"none",strokeLinejoin:"round",strokeWidth:"32",rx:"48"},child:[]},{tag:"circle",attr:{cx:"296",cy:"232",r:"24"},child:[]},{tag:"circle",attr:{cx:"376",cy:"232",r:"24"},child:[]},{tag:"circle",attr:{cx:"296",cy:"312",r:"24"},child:[]},{tag:"circle",attr:{cx:"376",cy:"312",r:"24"},child:[]},{tag:"circle",attr:{cx:"136",cy:"312",r:"24"},child:[]},{tag:"circle",attr:{cx:"216",cy:"312",r:"24"},child:[]},{tag:"circle",attr:{cx:"136",cy:"392",r:"24"},child:[]},{tag:"circle",attr:{cx:"216",cy:"392",r:"24"},child:[]},{tag:"circle",attr:{cx:"296",cy:"392",r:"24"},child:[]},{tag:"path",attr:{fill:"none",strokeLinecap:"round",strokeLinejoin:"round",strokeWidth:"32",d:"M128 48v32m256-32v32"},child:[]},{tag:"path",attr:{fill:"none",strokeLinejoin:"round",strokeWidth:"32",d:"M464 160H48"},child:[]}]})(c)}const j=()=>e.jsxs(h,{children:[e.jsx(g,{children:"Water consumption tracker"}),e.jsx(f,{children:"Record daily water intake and track"}),e.jsxs(m,{children:[e.jsx(u,{children:"Tracker Benefits"}),e.jsxs(k,{children:[e.jsxs(r,{children:[e.jsx(o,{children:e.jsx(y,{size:"100%",color:"#407BFF"})}),e.jsx(s,{children:"Habit drive"})]}),e.jsxs(r,{children:[e.jsx(o,{children:e.jsx(l,{size:"100%",color:"#407BFF"})}),e.jsx(s,{children:"View statistics"})]}),e.jsxs(r,{children:[e.jsx(o,{children:e.jsx(x,{size:"100%",color:"#407BFF"})}),e.jsx(s,{children:"Personal rate setting"})]})]})]}),e.jsx(p,{to:"/signup",children:e.jsx(b,{type:"button",children:"Try tracker"})})]}),w=t.div`
+`;function y(c){return d({tag:"svg",attr:{viewBox:"0 0 512 512"},child:[{tag:"rect",attr:{width:"416",height:"384",x:"48",y:"80",fill:"none",strokeLinejoin:"round",strokeWidth:"32",rx:"48"},child:[]},{tag:"circle",attr:{cx:"296",cy:"232",r:"24"},child:[]},{tag:"circle",attr:{cx:"376",cy:"232",r:"24"},child:[]},{tag:"circle",attr:{cx:"296",cy:"312",r:"24"},child:[]},{tag:"circle",attr:{cx:"376",cy:"312",r:"24"},child:[]},{tag:"circle",attr:{cx:"136",cy:"312",r:"24"},child:[]},{tag:"circle",attr:{cx:"216",cy:"312",r:"24"},child:[]},{tag:"circle",attr:{cx:"136",cy:"392",r:"24"},child:[]},{tag:"circle",attr:{cx:"216",cy:"392",r:"24"},child:[]},{tag:"circle",attr:{cx:"296",cy:"392",r:"24"},child:[]},{tag:"path",attr:{fill:"none",strokeLinecap:"round",strokeLinejoin:"round",strokeWidth:"32",d:"M128 48v32m256-32v32"},child:[]},{tag:"path",attr:{fill:"none",strokeLinejoin:"round",strokeWidth:"32",d:"M464 160H48"},child:[]}]})(c)}const j=()=>e.jsxs(h,{children:[e.jsx(g,{children:"Water consumption tracker"}),e.jsx(f,{children:"Record daily water intake and track"}),e.jsxs(m,{children:[e.jsx(u,{children:"Tracker Benefits"}),e.jsxs(b,{children:[e.jsxs(o,{children:[e.jsx(r,{children:e.jsx(y,{size:"100%",color:"#407BFF"})}),e.jsx(s,{children:"Habit drive"})]}),e.jsxs(o,{children:[e.jsx(r,{children:e.jsx(l,{size:"100%",color:"#407BFF"})}),e.jsx(s,{children:"View statistics"})]}),e.jsxs(o,{children:[e.jsx(r,{children:e.jsx(x,{size:"100%",color:"#407BFF"})}),e.jsx(s,{children:"Personal rate setting"})]})]})]}),e.jsx(p,{to:"/signup",children:e.jsx(k,{type:"button",children:"Try tracker"})})]}),w=t.div`
   display: flex;
   flex-direction: column;
   padding: 16px 24px;
@@ -135,7 +135,7 @@ import{n as t,G as d,j as e,H as l,a as x,L as p,F as n}from"./index-08674648.js
     width: 494px;
     padding: 32px 24px;
   }
-`,F=t.div`
+`,v=t.div`
   color: #2f2f2f;
   font-family: Roboto;
   font-size: 18px;
@@ -148,7 +148,7 @@ import{n as t,G as d,j as e,H as l,a as x,L as p,F as n}from"./index-08674648.js
   flex-direction: column;
   justify-content: center;
   align-items: start;
-`,i=t.li`
+`,n=t.li`
   display: flex;
   gap: 8px;
   justify-content: center;
@@ -165,14 +165,14 @@ import{n as t,G as d,j as e,H as l,a as x,L as p,F as n}from"./index-08674648.js
   @media only screen and (min-width: 768px) {
     width: 430px;
   }
-`,B=()=>e.jsxs(w,{children:[e.jsx(F,{children:"Why drink water"}),e.jsxs(M,{children:[e.jsxs(i,{children:[e.jsx(n,{size:"8px",color:"#407BFF"}),e.jsx(a,{children:"Supply of nutrients to all organs"})]}),e.jsxs(i,{children:[e.jsx(n,{size:"8px",color:"#407BFF"}),e.jsx(a,{children:"Providing oxygen to the lungs"})]}),e.jsxs(i,{children:[e.jsx(n,{size:"8px",color:"#407BFF"}),e.jsx(a,{children:"Maintaining the work of the heart"})]}),e.jsxs(i,{children:[e.jsx(n,{size:"8px",color:"#407BFF"}),e.jsx(a,{children:"Release of processed substances"})]}),e.jsxs(i,{children:[e.jsx(n,{size:"8px",color:"#407BFF"}),e.jsx(a,{children:"Ensuring the stability of the internal environment"})]}),e.jsxs(i,{children:[e.jsx(n,{size:"8px",color:"#407BFF"}),e.jsx(a,{children:"Maintaining within the normal temperature"})]}),e.jsxs(i,{children:[e.jsx(n,{size:"8px",color:"#407BFF"}),e.jsx(a,{children:"Maintaining an immune system capable of resisting disease"})]})]})]}),z="/Smart-Foxes-WaterTracker/assets/backgroundMainPageMob-48ec0365.png",T="/Smart-Foxes-WaterTracker/assets/backgroundMainPageMob2x@2-1f98ad26.png",P="/Smart-Foxes-WaterTracker/assets/backgroundMainPageTab-deaea3da.png",W="/Smart-Foxes-WaterTracker/assets/backgroundMainPageTab2x@2-52cf141b.png",v="/Smart-Foxes-WaterTracker/assets/backgroundMainPageDesk-599d04e6.png",$="/Smart-Foxes-WaterTracker/assets/backgroundMainPageDesk2x@2x-50dc0acd.png",L="/Smart-Foxes-WaterTracker/assets/backgroundElementMainPageDesk-4890b716.png",S="/Smart-Foxes-WaterTracker/assets/backgroundElementMainPageDesk2x@2-b22da114.png",R=t.div`
+`,B=()=>e.jsxs(w,{children:[e.jsx(v,{children:"Why drink water"}),e.jsxs(M,{children:[e.jsxs(n,{children:[e.jsx(i,{size:"8px",color:"#407BFF"}),e.jsx(a,{children:"Supply of nutrients to all organs"})]}),e.jsxs(n,{children:[e.jsx(i,{size:"8px",color:"#407BFF"}),e.jsx(a,{children:"Providing oxygen to the lungs"})]}),e.jsxs(n,{children:[e.jsx(i,{size:"8px",color:"#407BFF"}),e.jsx(a,{children:"Maintaining the work of the heart"})]}),e.jsxs(n,{children:[e.jsx(i,{size:"8px",color:"#407BFF"}),e.jsx(a,{children:"Release of processed substances"})]}),e.jsxs(n,{children:[e.jsx(i,{size:"8px",color:"#407BFF"}),e.jsx(a,{children:"Ensuring the stability of the internal environment"})]}),e.jsxs(n,{children:[e.jsx(i,{size:"8px",color:"#407BFF"}),e.jsx(a,{children:"Maintaining within the normal temperature"})]}),e.jsxs(n,{children:[e.jsx(i,{size:"8px",color:"#407BFF"}),e.jsx(a,{children:"Maintaining an immune system capable of resisting disease"})]})]})]}),z="/react_vite/assets/backgroundMainPageMob-48ec0365.png",F="/react_vite/assets/backgroundMainPageMob2x@2-1f98ad26.png",P="/react_vite/assets/backgroundMainPageTab-deaea3da.png",$="/react_vite/assets/backgroundMainPageTab2x@2-52cf141b.png",L="/react_vite/assets/backgroundMainPageDesk-599d04e6.png",T="/react_vite/assets/backgroundMainPageDesk2x@2x-50dc0acd.png",W="/react_vite/assets/backgroundElementMainPageDesk-4890b716.png",R="/react_vite/assets/backgroundElementMainPageDesk2x@2-b22da114.png",D=t.div`
   display: flex;
   flex-direction: column;
   gap: 40px;
   padding: 24px 20px 40px;
   background-image: image-set(
     url(${z}) 1x,
-    url(${T}) 2x
+    url(${F}) 2x
   );
   background-repeat: no-repeat;
   background-position: center;
@@ -180,19 +180,19 @@ import{n as t,G as d,j as e,H as l,a as x,L as p,F as n}from"./index-08674648.js
   @media only screen and (min-width: 768px) {
     background-image: image-set(
       url(${P}) 1x,
-      url(${W}) 2x
+      url(${$}) 2x
     );
     gap: 60px;
     padding: 40px 32px 50px;
   }
   @media screen and (min-width: 1440px) {
     background-image: image-set(
-        url(${v}) 1x,
-        url(${$}) 2x
+        url(${L}) 1x,
+        url(${T}) 2x
       ),
       image-set(
-        url(${L}) 1x,
-        url(${S}) 2x
+        url(${W}) 1x,
+        url(${R}) 2x
       );
     flex-direction: row;
     align-items: flex-end;
@@ -204,4 +204,4 @@ import{n as t,G as d,j as e,H as l,a as x,L as p,F as n}from"./index-08674648.js
     padding: 80px 0 294px;
     justify-content: center;
   }
-`,I=()=>e.jsxs(R,{children:[e.jsx(j,{}),e.jsx(B,{})]});export{I as default};
+`,I=()=>e.jsxs(D,{children:[e.jsx(j,{}),e.jsx(B,{})]});export{I as default};
---
+++ assets/NotFoundPage-29883bc4.js
@@ -0,0 +1,1 @@
+import{j as o}from"./index-b1d93bb4.js";const e=()=>o.jsx("div",{children:"NotFoundPage"});export{e as default};
