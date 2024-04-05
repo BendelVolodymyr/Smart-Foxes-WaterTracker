d4ec4eb2339abc1c042bdc1ac569f25b375e740f
NataFilina <filinanatash@gmail.com>
2024-04-05 22:19:03 +0300

    update styles
    зробила фіксований розмір котейнера для контетнту
    ховер і актів на кнопку

--- src/pages/Main/Main.styled.jsx
+++ src/pages/Main/Main.styled.jsx
@@ -11,23 +11,18 @@ import backgroundElementMainPageDesk2x from '../../assets/backgroundWelcomePg/ba
 
 export const Wrapper = styled.div`
   display: flex;
-  flex-direction: column;
-  gap: 40px;
-  padding: 24px 20px 40px;
-  background-image: image-set(
-    url(${backgroundMainPageMob}) 1x,
-    url(${backgroundMainPageMob2x}) 2x
-  );
+  justify-content: center;
+  align-items: center;
+  background-image: image-set(url(${backgroundMainPageMob}) 1x, url(${backgroundMainPageMob2x}) 2x);
   background-repeat: no-repeat;
   background-position: center;
   background-size: cover;
+
   @media only screen and (min-width: 768px) {
     background-image: image-set(
       url(${backgroundMainPageTab}) 1x,
       url(${backgroundMainPageTab2x}) 2x
     );
-    gap: 60px;
-    padding: 40px 32px 50px;
   }
   @media screen and (min-width: 1440px) {
     background-image: image-set(
@@ -38,14 +33,28 @@ export const Wrapper = styled.div`
         url(${backgroundElementMainPageDesk}) 1x,
         url(${backgroundElementMainPageDesk2x}) 2x
       );
+  }
+`;
+
+export const WrapperContent = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 40px;
+  padding: 24px 20px 40px;
+  width: 280px;
+  justify-content: center;
+  align-items: center;
+  @media only screen and (min-width: 768px) {
+    width: 704px;
+    align-items: flex-start;
+    gap: 60px;
+    padding: 40px 32px 50px;
+  }
+  @media screen and (min-width: 1440px) {
+    width: 1014px;
     flex-direction: row;
     align-items: flex-end;
     gap: 81px;
-    background-size: contain;
-    background-position:
-      right bottom,
-      left 49px;
     padding: 80px 0 294px;
-    justify-content: center;
   }
 `;
