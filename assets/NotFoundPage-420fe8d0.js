2fd642f8e1b988ed2046dfd874db116c834ded22
BendelVolodymyr <134164183+BendelVolodymyr@users.noreply.github.com>
2024-04-05 10:00:49 +0000

    Deploying to gh-pages from @ BendelVolodymyr/Smart-Foxes-WaterTracker@8e760cd6d267b31fd80ca0bbc2865dd9f1f8a11e 🚀

--- assets/NotFoundPage-420fe8d0.js
+++
@@ -1,1 +0,0 @@
-import{j as o}from"./index-a5b9d453.js";const e=()=>o.jsx("div",{children:"NotFoundPage"});export{e as default};
---
+++ assets/NotFoundPage-d336414e.js
@@ -0,0 +1,1 @@
+import{j as o}from"./index-20b57e30.js";const e=()=>o.jsx("div",{children:"NotFoundPage"});export{e as default};
