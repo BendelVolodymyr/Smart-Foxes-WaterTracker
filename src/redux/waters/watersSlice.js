7c9e6b871bc7de8fc0d4dd9e7fd4a31c772b2cb8
OksanaLesjuk <126483028+OksanaLesjuk@users.noreply.github.com>
2024-04-05 13:38:57 +0300

    Update watersSlice.js

--- src/redux/waters/watersSlice.js
+++ src/redux/waters/watersSlice.js
@@ -62,7 +62,7 @@ const waterSlice = createSlice({
       .addCase(API.portionsPerDay.rejected, handleRejected)
       .addCase(API.portionsPerMonth.pending, handlePending)
       .addCase(API.portionsPerMonth.fulfilled, (state, action) => {
-        state.waterMounthList = action.payload.data.monthData;
+        state.waterMounthList = action.payload.data;
       })
       .addCase(API.portionsPerMonth.rejected, handleRejected);
   },
