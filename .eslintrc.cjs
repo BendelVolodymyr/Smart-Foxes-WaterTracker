402db326e3b12604089dbe3701e439b22bfe918f
BendelVolodymyr <bendel.vova@gmail.com>
2024-04-05 15:25:48 +0200

    new Config and prettierrc

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,38 +1,22 @@
 module.exports = {
   root: true,
-  env: {
-    browser: true,
-    es2020: true,
-  },
+  env: { browser: true, es2020: true },
   extends: [
     'eslint:recommended',
     'plugin:react/recommended',
     'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',
-    'plugin:prettier/recommended',
+    'prettier',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
-  },
-  settings: {
-    react: {
-      version: '18.2',
-    },
-  },
+  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+  settings: { react: { version: '18.2' } },
   plugins: ['react-refresh'],
   rules: {
     'react/prop-types': 'off',
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
-    ],
-    'prettier/prettier': [
-      'warn',
-      {
-        endOfLine: 'auto',
-      },
     ],
   },
 };
--- vite.config.js
+++ vite.config.js
@@ -12,5 +12,5 @@ export default defineConfig({
       assets: '/src/assets',
     },
   },
-  base: '/Smart-Foxes-WaterTracker/',
+  base: '/react_vite/',
 });
