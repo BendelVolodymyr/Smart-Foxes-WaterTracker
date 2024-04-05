ee40d952d229ea849abb73d4765d8e92c1b520d5
BendelVolodymyr <bendel.vova@gmail.com>
2024-04-05 16:31:42 +0200

    fixed

--- src/components/HomeWaterPageComponents/MonthStateTable/MonthStateTable.jsx
+++ src/components/HomeWaterPageComponents/MonthStateTable/MonthStateTable.jsx
@@ -1,16 +1,26 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import DatePaginator from '../DatePaginator/DatePaginator';
-
 import Popover from '@mui/material/Popover';
 import DaysGeneralStats from '../DaysGeneralStats/DaysGeneralStats';
+import { startOfMonth, endOfMonth, eachDayOfInterval } from 'date-fns';
+import { useDispatch } from 'react-redux';
 import {
   MonthComponentWrapper,
   PaginatorBlock,
 } from './MonthStateTable.styled';
-import DaysList from '../DayList/DayList';
+import {
+  DayCell,
+  DayCircle,
+  DayNumber,
+  DayPercentage,
+  DaysGrid,
+} from './DayGreed.styled';
+import { portionsPerMonth } from '../../../redux/waters/operations';
+import useWater from '../../../hooks/useWaters';
 
 const MonthStateTable = () => {
   const [selectedDate, setSelectedDate] = useState(new Date());
+  const [currentMonth, setCurrentMonth] = useState(selectedDate.getMonth());
   const [anchorEl, setAnchorEl] = useState(null);
   const dispatch = useDispatch();
   const monthData = useWater().waterMonthList;
@@ -28,20 +38,22 @@ const MonthStateTable = () => {
     const endOfMonthDate = endOfMonth(selectedDate);
 
     setCurrentMonth(selectedDate.getMonth());
-    dispatch(portionsPerMonth({ startDate: startOfMonthDate, endDate: endOfMonthDate }));
-  }, [currentMonth]);
+    dispatch(
+      portionsPerMonth({ startDate: startOfMonthDate, endDate: endOfMonthDate })
+    );
+  }, [currentMonth, dispatch, selectedDate]);
 
   const handleDayClick = (event, date) => {
     setSelectedDate(date);
     setAnchorEl(event.currentTarget);
   };
 
-  const handleClosePopover = () => {
-    setAnchorEl(null);
-  };
-
-  const open = Boolean(anchorEl);
-  const id = open ? 'simple-popover' : undefined;
+  const selectedDayData =
+    monthData &&
+    monthData.find(dayData => {
+      const [dayOfMonth] = dayData.date.split(',');
+      return parseInt(dayOfMonth) === selectedDate.getDate();
+    });
 
   return (
     <MonthComponentWrapper>
@@ -50,10 +62,35 @@ const MonthStateTable = () => {
         <DatePaginator
           selectedDate={selectedDate}
           setSelectedDate={setSelectedDate}
+          setCurrentMonth={setCurrentMonth}
         />
       </PaginatorBlock>
 
-      <DaysList selectedDate={selectedDate} onDayClick={handleDayClick} />
+      {/* <DaysGrid>{days}</DaysGrid> */}
+
+      <DaysGrid>
+        {eachDayOfInterval({
+          start: startOfMonth(selectedDate),
+          end: endOfMonth(selectedDate),
+        }).map((day, index) => {
+          const dayOfMonth = day.getDate();
+          const dayData = monthData.find(data => {
+            const [dayNum] = data.date.split(',');
+            return parseInt(dayNum) === dayOfMonth;
+          });
+          const percentagePerDay = dayData ? dayData.percentagePerDay : null;
+
+          return (
+            <DayCell key={index} onClick={event => handleDayClick(event, day)}>
+              <DayCircle percentage={percentagePerDay}>
+                <DayNumber>{dayOfMonth}</DayNumber>
+              </DayCircle>
+              <DayPercentage>{percentagePerDay || 0}%</DayPercentage>
+            </DayCell>
+          );
+        })}
+      </DaysGrid>
+
       <Popover
         id={id}
         open={open}
@@ -68,7 +105,10 @@ const MonthStateTable = () => {
           horizontal: 'right',
         }}
       >
-        <DaysGeneralStats selectedDate={selectedDate} />
+        <DaysGeneralStats
+          selectedDate={selectedDate}
+          selectedDayData={selectedDayData}
+        />
       </Popover>
     </MonthComponentWrapper>
   );
--- src/components/NotAuthNav/NotAuthNav.jsx
+++
@@ -1,5 +0,0 @@
-import { Link } from "react-router-dom";
-
-export const NotAuthNav = () => {
-  return <div><Link to="/login" >Login</Link><Link to='/registration' >Registration</Link></div>;
-};  
