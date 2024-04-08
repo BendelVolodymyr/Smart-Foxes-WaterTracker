5d82033e45282efb09f9112ca25ad95374eea093
BendelVolodymyr <bendel.vova@gmail.com>
2024-04-08 13:47:41 +0200

    not found

--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -1,5 +1,21 @@
+import NotFound from '../assets/NotFound/uG73t6y3S6obzxxr4w.gif';
+
 const NotFoundPage = () => {
-  return <div>NotFoundPage</div>;
+  return (
+    <>
+      <img
+        style={{
+          display: 'flex',
+          alignItems: 'center',
+          margin: 'auto',
+          width: '500px',
+          height: 'auto',
+        }}
+        src={NotFound}
+        alt="NotFound"
+      />
+    </>
+  );
 };
 
 export default NotFoundPage;
