8c5b2a77c120ffcc081b18a149304192ea39a3df
BendelVolodymyr <bendel.vova@gmail.com>
2024-04-10 16:59:40 +0200

    fixed test

--- src/components/Particles/Particles.jsx
+++ src/components/Particles/Particles.jsx
@@ -19,7 +19,14 @@ const ParticlesComponent = props => {
 
   const options = useMemo(() => particlesConfig, []);
 
-  return <Particles id={props.id} init={particlesLoaded} options={options} />;
+  return (
+    <Particles
+      style={{ position: 'absolute' }}
+      id={props.id}
+      init={particlesLoaded}
+      options={options}
+    />
+  );
 };
 
 export default ParticlesComponent;
